6f51e754e9bb19c1e1248abbfd99609f9a46e230
Lou BEGE <wolfla@hotmail.fr>
2021-06-28 15:20:44 +0200

    Add comments

--- ETL_EDAMAM_NUTRITIONIX.py
+++ ETL_EDAMAM_NUTRITIONIX.py
@@ -9,35 +9,47 @@ from elasticsearch import Elasticsearch
 class ETL_EDAMAM_NUTRITIONIX:
     def __init__(self):
         self.listOfFood = self.get_foodname()
+        print(self.listOfFood)
         self.listOfNutrients = self.get_nutrients()
         self.elasticsearch = Elasticsearch(
             cloud_id=config('ELASTIC_CLOUD_ID'),
             http_auth=(config('ELASTIC_USERNAME'), config('ELASTIC_PASSWORD')),
         )
     
+    ###
+    # Récupération de la liste des nutriments
+    ###
     def get_nutrients(self):
+        # Initialisation de l'appel à l'API
         ID = config('NUTRITIONAPI_SECRET_ID')
         KEY = config('NUTRITIONAPI_SECRET_KEY')
         headers = {"x-app-id":ID, "x-app-key": KEY, "x-remote-user-id": "0"}
         URL = 'https://trackapi.nutritionix.com/v2/utils/nutrients'
         req = requests.get(URL, headers=headers)
+        
         ## Vérification que le code est bien 200
         if req.status_code == 200:
-            print(req.json())
             return req.json()
 
+    ###
+    # Récupération d'une liste de noms de nourriture
+    ###
     def get_foodname(self):
+        # Initialisation de l'appel à l'API
         ID = config('FOODNAMEAPI_SECRET_ID')
         KEY = config('FOODNAMEAPI_SECRET_KEY')
+        
         ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         foodname = []
         for i in range(len(ALPHABET)):
             URL = f"https://api.edamam.com/auto-complete?app_id={ID}&app_key={KEY}&q={ALPHABET[i]}"
             req = requests.get(URL)
+            
             ## Vérification que le code est bien 200
             if req.status_code == 200:
                 foodname = foodname+req.json()
-        return foodname
+        
+        return list(dict.fromkeys(foodname))
 
     ###
     # Ne prend pas d'input
@@ -46,15 +58,14 @@ class ETL_EDAMAM_NUTRITIONIX:
     # Ce sont les données sur lesquelles on va appliquer des transformations
     ###
     def extract_data(self):
-        # self.elasticsearch.delete_by_query(index="products", body={"query": {"match_all": {}}})
         ID = config('NUTRITIONAPI_SECRET_ID')
         KEY = config('NUTRITIONAPI_SECRET_KEY')
         headers = {"x-app-id":ID, "x-app-key": KEY, "x-remote-user-id": "0"}
         # for i in range(len(self.listOfFood)):
         for i in range(1):
             URL = f'https://trackapi.nutritionix.com/v2/search/instant?query={self.listOfFood[i]}&branded=true&common=false&detailed=true&claims=true'
-            print(URL)
             req = requests.get(URL, headers=headers)
+
             ## Vérification que le code est bien 200
             if req.status_code == 200:
                 json = req.json()
@@ -68,7 +79,6 @@ class ETL_EDAMAM_NUTRITIONIX:
     # Contient toutes les règles de transformation à appliquer sur les données
     ###
     def transform(self,*args):
-        
         # Itération sur chaque nutriment pour récupérer les labels correspondant
         args[0]["new_nutrients"] = []
         for k in range(len(args[0]["full_nutrients"])):
@@ -90,26 +100,33 @@ class ETL_EDAMAM_NUTRITIONIX:
         KEY_RECIPE = config('RECIPEAPI_SECRET_KEY')
         ID_FOODNAME = config('FOODNAMEAPI_SECRET_ID')
         KEY_FOODNAME = config('FOODNAMEAPI_SECRET_KEY')
+        
         ## Appel de l'API des recettes
         URL = f"https://api.edamam.com/api/recipes/v2?app_id={ID_RECIPE}&app_key={KEY_RECIPE}&type=public&q={args[0]['food_name']}"
         req = requests.get(URL)
+        
         ## Vérification que le code est bien 200
         if req.status_code == 200:
             jsonrecipe = req.json()
+            
             ## Vérification qu'il y a bien quelque chose qui existe
             if jsonrecipe["hits"]:
                 ## Récupération des ingrédients
                 ingredients = jsonrecipe["hits"][0]["recipe"]["ingredients"]
+               
                 ## Pour chaque ingrédient
                 for i in range(len(ingredients)):
                     ## Récupération de l'id de l'ingrédient
                     foodId = ingredients[i]["foodId"]
+                   
                     # Appel de l'API de la nourriture à partir de l'id de l'ingrédient
                     URL = f"https://api.edamam.com/api/food-database/v2/parser?app_id={ID_FOODNAME}&app_key={KEY_FOODNAME}&ingr={foodId}"
                     req = requests.get(URL)
+                    
                     ## Vérification que le code est bien 200
                     if req.status_code == 200:
                         jsonfooddb = req.json()
+                        
                         ## Récupération du nom de l'ingrédient
                         nameIngr = jsonfooddb["hints"][0]["food"]["label"]
                         args[0]['ingredients'].append(nameIngr)
@@ -117,6 +134,7 @@ class ETL_EDAMAM_NUTRITIONIX:
         # Ajout de la source
         args[0]["source_api"] = Source.EDAMAM_NUTRITIONIX.name
 
+        # Récupération de "FR" / "US" ...etc
         args[0]['locale'] = args[0]['locale'].split("_")[1]
 
         # Suppression des données inutiles
@@ -138,11 +156,12 @@ class ETL_EDAMAM_NUTRITIONIX:
     # Ne retourne absolument rien
     ###
     def load(self,*args):
-        print("------------")    
+        print(f"----- {args[0]['food_name']} -----")    
         # Check si ça existe en BDD
         result = self.elasticsearch.search(index="products", body={"query":{"match": {"product": args[0]['food_name']}}})
+        
         # Si non, insert en BDD
-        print(result["hits"]["hits"])
+        # print(result["hits"]["hits"])
         if not result["hits"]["hits"]:
             product = {
                 "from": args[0]['locale'],
@@ -154,12 +173,9 @@ class ETL_EDAMAM_NUTRITIONIX:
                 "ingredients": args[0]['ingredients'],
                 "nutriscore": args[0]['nutriscore']
             }
-            print("*** To insert ***")
-            print(product)
-            response = self.elasticsearch.index(index="products", body=product)
-            print(response['result'])
-        # print(args[0])
+            self.elasticsearch.index(index="products", body=product)
         time.sleep(5)
+    
     ###
     # Permet de configurer le graphique Bonobo
     # Cette configuration est totalement personnalisable
